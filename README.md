2bf564fc3eb75768535dbc7d19b1554c06b215fc
Mucahit Cigci <cmucayit@gmail.com>
2022-12-18 18:29:58 +0300

    todo list

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<!-- for uniq id  -->
+
+# npm install uuid
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const Form = ({ input, setInput, todos, setTodos }) => {
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+    setInput("");
+    console.log(setInput);
+  };
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter todo"
+        className="task-input"
+        value={input}
+        required
+        onChange={onInputChange}
+      />
+      <button className="button-add" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default Form;
